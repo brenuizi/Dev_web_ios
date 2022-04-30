7379888c5376658919f85b81b0d6c6e352e7875b
Breno <brenovs12@gmail.com>
2022-04-29 22:48:57 -0300

    subindo arquivos

---
+++ JS/Aula02_VariaveisOperadores/Exercício/main.js
@@ -0,0 +1,10 @@
+const nome = "Breno"; 
+const peso = 53;
+const altura = 1.67;
+const IMC = peso / (altura * altura)
+console.log(IMC);
+console.log(
+    `${nome} possui ${altura}m e peso ${peso}kg, seu IMC é ${IMC}`
+)
+
+
