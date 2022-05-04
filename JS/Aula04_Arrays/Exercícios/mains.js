a8ded0580cb7510902e34b027a897beb9fa364c4
brenuizi <brenovs12@gmail.com>
2022-05-03 21:51:59 -0300

    subindo arquivos

---
+++ JS/Aula04_Arrays/Exercícios/mains.js
@@ -0,0 +1,38 @@
+const names = ['Joaquim', 'José', 'Silva', 'Xavier'];
+
+const names1 = ['Joaquim', 'José', 'Silva', 'Xavier'];
+let one = names1.unshift('Amanda'); // one = 5
+
+console.log(names1);
+console.log(one);
+
+console.clear();
+const names2 = ['Amanda', 'Joaquim', 'José', 'Silva', 'Xavier'];
+let two = names2.pop('Xavier');
+
+console.log(names2);
+console.log(two);
+
+console.clear();
+const names3 = ['Amanda', 'Joaquim', 'José', 'Silva'];
+let three = names3.push('Zuleica');
+
+console.log(names3);
+console.log(three);
+
+console.clear
+const names4 = ['Amanda', 'Joaquim', 'José', 'Silva','Zuleica'];
+let four = names4.shift('Amanda');
+
+console.log(names4);
+console.log(four);
+
+console.clear
+const names5 = ['Joaquim', 'José', 'Silva', 'Zuleica'];
+names5 [2] = 'Silvana';
+
+console.log(names5);
+
+
+
+var series
