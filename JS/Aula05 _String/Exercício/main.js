142b399f5c550cf26ae87634f1f153ecbb4eef95
brenuizi <brenovs12@gmail.com>
2022-05-10 21:48:27 -0300

    subindo arquivos

---
+++ JS/Aula05 _String/Exercício/main.js
@@ -0,0 +1,17 @@
+let LeroLero = 'Dr Stephen Strange lança um feitiço proibido que abre um portal para o multiverso. No entanto, surge uma ameaça que pode ser grande demais para sua equipe lidar.'
+console.log(LeroLero.charAt([0]));
+
+let LeroLero1 = 'Dr Stephen Strange lança um feitiço proibido que abre um portal para o multiverso. No entanto, surge uma ameaça que pode ser grande demais para sua equipe lidar.'
+console.log(LeroLero1.length);
+
+let LeroLero2 = 'Dr Stephen Strange lança um feitiço proibido que abre um portal para o multiverso. No entanto, surge uma ameaça que pode ser grande demais para sua equipe lidar.'
+console.log('O tamanho da String é');
+console.log(LeroLero2.length);
+
+let LeroLero3 = 'Dr Stephen Strange lança um feitiço proibido que abre um portal para o multiverso. No entanto, surge uma ameaça que pode ser grande demais para sua equipe lidar.'
+let LeroLero4 = LeroLero3.replace ('Dr Stephen Strange' , 'Feiticeira Escarlate');
+console.log(LeroLero4);
+
+let LeroLero5 = 'Feiticeira Escarlate lança um feitiço proibido que abre um portal para o multiverso. No entanto, surge uma ameaça que pode ser grande demais para sua equipe lidar.'
+console.log()
+console.log(LeroLero5.charAt([13]));
---
+++ JS/Aula06_ArrayObjetos/Exercício/main.js
@@ -0,0 +1,59 @@
+const alunos = [
+    {id:1, 
+    nome: 'Breno',
+    idade: 20,
+    endereço: {
+        rua: 'Rua da alegria',
+        numero: 0,
+        cidade: 'São Paulo',
+        estado: 'SP',
+    },
+    notas: {
+        português: 10,
+        matemática: 8,
+        biologia: 9,
+        filosofia: 7,
+    }
+    }
+];
+console.log(alunos);
+console.log(alunos.notas)
+
+
+const alunos2 = [
+    {id:1, 
+    nome: 'Victória',
+    idade: 19,
+    endereço: {
+        rua: 'Rua da amargura',
+        numero: 10,
+        cidade: 'São Paulo',
+        estado: 'SP',
+    },
+    notas: {
+        português: 7,
+        matemática: 6,
+        biologia: 5,
+        filosofia: 5,
+    }
+    }
+];
+
+const alunos3 = [
+    {id:1, 
+    nome: 'Jacob',
+    idade: 22,
+    endereço: {
+        rua: 'Rua da indecisão',
+        numero: 18,
+        cidade: 'São Paulo',
+        estado: 'SP',
+    },
+    notas: {
+        português: 9,
+        matemática: 7,
+        biologia: 8,
+        filosofia: 9,
+    }
+    }
+];
