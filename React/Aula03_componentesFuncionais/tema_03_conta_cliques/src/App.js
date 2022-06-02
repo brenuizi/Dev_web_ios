6691f281c417dc73ec7a94c14448800f4385967b
brenuizi <brenovs12@gmail.com>
2022-06-01 21:52:18 -0300

    Subindo Aula 03 React

---
+++ React/Aula03_componentesFuncionais/tema_03_conta_cliques/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Exemplo from './Components/Exemplo';
+import Header from './Components/Header';
+
+function App() {
+    return (
+        <div className="App">
+            <Header nome='Breno' />
+            <Exemplo />
+        </div>
+    );
+}
+
+
+
+export default App;
