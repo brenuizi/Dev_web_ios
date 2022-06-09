e032a0590e44fcec28a6de57c237bb220bef0a81
brenuizi <brenovs12@gmail.com>
2022-06-08 21:49:13 -0300

    subindo arquivos React

---
+++ React/Aula03_componentesFuncionais/Exercício/exercicio_aula_03/src/Components/BoasVindas.jsx
@@ -0,0 +1,20 @@
+
+const BoasVindas = (props) => {
+    return (
+        <section>
+            <h1>Seja bem vindo(a)</h1>
+            <p>O React é uma biblioteca JavaScript de código aberto com foco em criar interfaces de usuário em páginas web. </p>
+        </section>
+    )
+
+};
+
+BoasVindas.defaultProps = {
+    nome: 'Nome Padrão'
+};
+
+BoasVindas.propTypes = {
+    nome: propTypes.string
+};
+
+export default BoasVindas;
