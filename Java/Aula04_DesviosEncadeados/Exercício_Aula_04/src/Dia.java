2b9307fb2329171a6fa39d651d8c4ed71397ba3f
brenuizi <brenovs12@gmail.com>
2022-07-11 18:48:53 -0300

    subindo exercícios de java

--- Java/Aula04_DesviosEncadeados/Exercício_Aula_04/src/Dia.java
+++ Java/Aula04_DesviosEncadeados/Exercício_Aula_04/src/Dia.java
@@ -4,30 +4,30 @@ public class Dia {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
 
-        System.out.println("Por favor, digite um número: ");
+        System.out.println("Para um dia da semana, digite um número: ");
         int diaSemana = entrada.nextInt();
 
         switch (diaSemana) {
             case 1:
-                System.out.println("Domingo");
+                System.out.println("1 é Domingo");
                 break;
                 case 2:
-                System.out.println("Segunda");
+                System.out.println("2 é Segunda-feira");
                 break;
                 case 3:
-                System.out.println("Terça");
+                System.out.println("3 é Terça-feira");
                 break;
                 case 4:
-                System.out.println("Quarta-feira");
+                System.out.println("4 é Quarta-feira");
                 break;
                 case 5:
-                System.out.println("Quinta-feira");
+                System.out.println("5 é Quinta-feira");
                 break;
                 case 6:
-                System.out.println("Sexta-feira");
+                System.out.println("6 é Sexta-feira");
                 break;
                 case 7:
-                System.out.println("Sábado");
+                System.out.println("7 é Sábado");
                 break;
 
             default:
