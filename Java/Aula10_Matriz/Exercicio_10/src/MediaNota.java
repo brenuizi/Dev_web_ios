fe1d61d392509e6a00e51915f68ffd6f23971145
brenuizi <brenovs12@gmail.com>
2022-07-12 21:31:40 -0300

    subindo exercício 10 de java atualizado

--- Java/Aula10_Matriz/Exercicio_10/src/MediaNota.java
+++
@@ -1,8 +0,0 @@
-public class MediaNota {
-    public static void main(String[] args) {
-        double nota1 = 10, nota2 = 8, nota3 = 7, nota4 = 9;
-        double media = ((nota1 + nota2 + nota3 + nota4) / 4);
-
-        System.out.print(media);
-    }
-}
